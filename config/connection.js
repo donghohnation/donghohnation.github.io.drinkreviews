82a9b9f095443d7ce08723fae63e28e0432dc3d2
donghohnation <donghoh.han@gmail.com>
2022-07-06 20:07:13 -0700

    another minor update

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ if (process.env.JAWSDB_URL) {
 } else {
   connection = mysql.createConnection({
     host: "localhost",
-    port: 3301,
+    port: 3306,
     user: "root",
     password: "chickenrecipe123",
     database: "drinks_DB"
