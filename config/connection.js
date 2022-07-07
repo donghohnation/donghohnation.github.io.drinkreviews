830c454b779e23f67c65207d6ab1758dca15cd7a
donghohnation <donghoh.han@gmail.com>
2022-07-06 22:39:52 -0700

    jaws amber

--- config/connection.js
+++ config/connection.js
@@ -3,8 +3,8 @@ const mysql = require('mysql');
 
 let connection;
 
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
+if (process.env.JAWSDB_AMBER) {
+  connection = mysql.createConnection(process.env.JAWSDB_AMBER);
 } else {
   connection = mysql.createConnection({
     host: "localhost",
