7e5bd85ae943168412c523462849b6bdd58b57e8
donghohnation <donghoh.han@gmail.com>
2022-07-06 22:38:14 -0700

    heroku fix?

--- config/connection.js
+++ config/connection.js
@@ -1,44 +1,28 @@
 // connect from node to mysql
-// const mysql = require('mysql');
-
-// let connection;
-
-// if (process.env.JAWSDB_URL) {
-//   connection = mysql.createConnection(process.env.JAWSDB_URL);
-// } else {
-//   connection = mysql.createConnection({
-//     host: "localhost",
-//     port: 3306,
-//     user: "root",
-//     password: "chickenrecipe123",
-//     database: "drinks_DB"
-//   })
-// };
-
-// // to make connection
-// connection.connect(err => {
-//   if (err) {
-//     console.error("error connecting: " + err.stack);
-//     return;
-//   }
-//   console.log("you are id " + connection.threadId);
-// });
+const mysql = require('mysql');
 
 let connection;
 
 if (process.env.JAWSDB_URL) {
-  connection = new Sequelize(process.env.JAWSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  connection = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PW,
-    {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306,
-    },
-  );
-}
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "chickenrecipe123",
+    database: "drinks_DB"
+  })
+};
+
+// to make connection
+connection.connect(err => {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("you are id " + connection.threadId);
+});
+
 //export connection
 module.exports = connection;
